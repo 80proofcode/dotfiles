2e1c260a92be6c717dad5d8fe8d709ed125ed570
80proofcode <amnoyb@gmail.com>
2016-04-03 15:55:28 -0400

    adding timers

--- aux/shell.helper.sh
+++ aux/shell.helper.sh
@@ -74,6 +74,23 @@ pdfman()
     bash -c "sleep 2; rm $TMPFILE" &
 }
 
+# shamelessly plagirized from SU
+function countdown(){
+   date1=$((`date +%s` + $1)); 
+   while [ "$date1" -ge `date +%s` ]; do 
+     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+     sleep 0.1
+   done
+}
+
+function stopwatch(){
+  date1=`date +%s`; 
+   while true; do 
+    echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r"; 
+    sleep 0.1
+   done
+}
+
 ## Functions - end
 
 printf "done\n"
